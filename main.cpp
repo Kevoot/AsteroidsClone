b818be5b85730a3990eb05b04460e21d2fa33e3b
UnsureSherlock <kevinholmes88@gmail.com>
2015-11-21 12:07:14 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/*
+  @author Kevin Holmes
+  @version 1.0 11/20/2015
+*/
+
+#include "GameEngine.hpp"
+
+int main()
+{
+  //Provided download for main, unmodified
+  GameEngine e("Asteroids!", 1500, 900);
+  e.runGame();
+  return 0;
+}
